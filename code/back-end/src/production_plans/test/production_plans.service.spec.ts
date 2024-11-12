41dc1bae9d4ba40e49e61a036cce335657177b2e
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2024-11-11 21:20:59 -0300

    #213 updating unit tests for production plans

--- code/back-end/src/production_plans/test/production_plans.service.spec.ts
+++ code/back-end/src/production_plans/test/production_plans.service.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Line } from 'src/line/entities/line.entity';
+import { Product } from 'src/products/entities/product.entity';
 import { Repository } from 'typeorm';
-import { Line } from '../../line/entities/line.entity';
-import { ProductionPlansService } from '../../production_plans/production_plans/production_plans.service';
-import { Product } from '../../products/entities/product.entity';
 import { ProductionPlan } from '../entities/production_plan.entity';
+import { ProductionPlansService } from '../production_plans/production_plans.service';
 
 describe('ProductionPlansService', () => {
   let service: ProductionPlansService;
@@ -14,6 +14,8 @@ describe('ProductionPlansService', () => {
 
   const mockProductionPlansRepository = {
     createQueryBuilder: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
   };
   const mockProductsRepository = {
     findOne: jest.fn(),
@@ -51,96 +53,77 @@ describe('ProductionPlansService', () => {
     linesRepository = module.get<Repository<Line>>(getRepositoryToken(Line));
   });
 
-  describe('findByDates', () => {
-    it('deve retornar planos de produção dentro do intervalo de datas', async () => {
-      const startDate = '2024-01-01';
-      const endDate = '2024-12-31';
-
-      const mockQueryBuilder = {
-        leftJoinAndSelect: jest.fn().mockReturnThis(),
-        andWhere: jest.fn().mockReturnThis(),
-        getMany: jest.fn().mockResolvedValue([
-          {
-            id: '1',
-            qtd: 10,
-            datePrev: new Date('2024-06-15'),
-            product: { id: 'product123', description: 'Product A' },
-            line: { lineId: 'line123', name: 'Line A' },
-            productions: [],
-          } as ProductionPlan,
-        ]),
+  describe('findOneById', () => {
+    it('deve retornar um plano de produção pelo ID', async () => {
+      const productionPlanId = '1';
+      const mockProductionPlan: ProductionPlan = {
+        id: productionPlanId,
+        qtd: 10,
+        datePrev: new Date('2024-06-15'),
+        product: { id: 'product123', description: 'Product A' } as Product,
+        line: { lineId: 'line123', name: 'Line A' } as Line,
+        productions: [],
       };
 
       jest
-        .spyOn(productionPlansRepository, 'createQueryBuilder')
-        .mockImplementation(() => mockQueryBuilder as any);
+        .spyOn(productionPlansRepository, 'findOne')
+        .mockResolvedValue(mockProductionPlan);
 
-      const result = await service.findByDates(startDate, endDate);
+      const result = await service.findOneById(productionPlanId);
 
-      expect(productionPlansRepository.createQueryBuilder).toHaveBeenCalledWith(
-        'productionPlan',
+      expect(productionPlansRepository.findOne).toHaveBeenCalledWith({
+        where: { id: productionPlanId },
+        relations: ['product'],
+      });
+      expect(result).toEqual(mockProductionPlan);
+    });
+
+    it('deve lançar um NotFoundException se o plano de produção não for encontrado', async () => {
+      const productionPlanId = '2';
+      jest.spyOn(productionPlansRepository, 'findOne').mockResolvedValue(null);
+
+      await expect(service.findOneById(productionPlanId)).rejects.toThrow(
+        'Production Plan with ID 2 not found',
       );
-      expect(mockQueryBuilder.leftJoinAndSelect).toHaveBeenCalledWith(
-        'productionPlan.product',
-        'product',
-      );
-      expect(mockQueryBuilder.leftJoinAndSelect).toHaveBeenCalledWith(
-        'productionPlan.line',
-        'line',
-      );
-      expect(mockQueryBuilder.andWhere).toHaveBeenCalledWith(
-        'productionPlan.datePrev >= :startDate',
-        { startDate },
-      );
-      expect(mockQueryBuilder.andWhere).toHaveBeenCalledWith(
-        'productionPlan.datePrev <= :endDate',
-        { endDate },
-      );
-      expect(result).toEqual([
-        {
-          id: '1',
-          qtd: 10,
-          datePrev: new Date('2024-06-15'),
-          product: { id: 'product123', description: 'Product A' },
-          line: { lineId: 'line123', name: 'Line A' },
-          productions: [],
-        },
-      ]);
     });
   });
 
-  // describe('findOneById', () => {
-  //   it('deve retornar um plano de produção pelo ID', async () => {
-  //     const productionPlanId = '1';
-  //     const mockProductionPlan: ProductionPlan = {
-  //       id: productionPlanId,
-  //       qtd: 10,
-  //       datePrev: new Date('2024-06-15'),
-  //       product: { id: 'product123', description: 'Product A' } as Product,
-  //       line: { lineId: 'line123', name: 'Line A' } as Line,
-  //       productions: [],
-  //     };
+  describe('remove', () => {
+    it('deve remover um plano de produção pelo ID', async () => {
+      const productionPlanId = '1';
+      const mockProductionPlan: ProductionPlan = {
+        id: productionPlanId,
+        qtd: 10,
+        datePrev: new Date('2024-06-15'),
+        product: { id: 'product123', description: 'Product A' } as Product,
+        line: { lineId: 'line123', name: 'Line A' } as Line,
+        productions: [],
+      };
 
-  //     jest
-  //       .spyOn(productionPlansRepository, 'findOne')
-  //       .mockResolvedValue(mockProductionPlan);
+      jest
+        .spyOn(productionPlansRepository, 'findOne')
+        .mockResolvedValue(mockProductionPlan);
+      jest
+        .spyOn(productionPlansRepository, 'delete')
+        .mockResolvedValue(undefined);
 
-  //     const result = await service.findOneById(productionPlanId);
+      await service.remove(productionPlanId);
 
-  //     expect(productionPlansRepository.findOne).toHaveBeenCalledWith({
-  //       where: { id: productionPlanId },
-  //       relations: ['product'],
-  //     });
-  //     expect(result).toEqual(mockProductionPlan);
-  //   });
+      expect(productionPlansRepository.findOne).toHaveBeenCalledWith({
+        where: { id: productionPlanId },
+      });
+      expect(productionPlansRepository.delete).toHaveBeenCalledWith(
+        productionPlanId,
+      );
+    });
 
-  //   it('deve lançar um NotFoundException se o plano de produção não for encontrado', async () => {
-  //     const productionPlanId = '2';
-  //     jest.spyOn(productionPlansRepository, 'findOne').mockResolvedValue(null);
+    it('deve lançar um erro se o plano de produção não for encontrado para remoção', async () => {
+      const productionPlanId = '2';
+      jest.spyOn(productionPlansRepository, 'findOne').mockResolvedValue(null);
 
-  //     await expect(service.findOneById(productionPlanId)).rejects.toThrow(
-  //       'Production Plan with ID 2 not found',
-  //     );
-  //   });
-  // });
+      await expect(service.remove(productionPlanId)).rejects.toThrow(
+        `ProductionPlan with id ${productionPlanId} not found`,
+      );
+    });
+  });
 });
