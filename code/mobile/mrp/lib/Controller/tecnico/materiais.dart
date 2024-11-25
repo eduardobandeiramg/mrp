da9debef6fa2c00267d0c1966713fff243f5b17f
eduardobandeiramg <eduardobandeiramg@gmail.com>
2024-11-25 15:22:05 -0300

    criando funcao de dar baixa no estoque

--- code/mobile/mrp/lib/Controller/tecnico/materiais.dart
+++ code/mobile/mrp/lib/Controller/tecnico/materiais.dart
@@ -47,7 +47,30 @@ class Materiais {
     }
   }
 
-  void tirarDoEstoque() async {
-    String urlTirarEstoque = "http://10.0.2.2:3000/";
+  void tirarDoEstoque(String idMaterial, int qtdARetirar) async {
+    int quantidadeAtual;
+    String urlConsultarMaterial = "http://10.0.2.2:3000/materials/$idMaterial";
+    String urlAtualizarEstoque = "http://10.0.2.2:3000/materials/stock/update";
+    http.Response respostaConsultarMaterial = await http.get(
+        Uri.parse(urlConsultarMaterial),
+        headers: {"Authorization": TokenApp.tokenApp!});
+    if (respostaConsultarMaterial.statusCode == 200) {
+      Map<String, dynamic> mapaMaterial =
+          Map<String, dynamic>.from(jsonDecode(respostaConsultarMaterial.body));
+      quantidadeAtual = mapaMaterial["qtd"];
+    } else {
+      throw new Exception("erro-ao-acessar-dados-material");
+    }
+    int novaQuantidade = quantidadeAtual - qtdARetirar;
+    print("quantidade atual : $quantidadeAtual");
+    print("nova quantidade : $novaQuantidade");
+    http.Response respostaAtualizarEstoque = await http
+        .patch(Uri.parse(urlAtualizarEstoque), headers: {
+      "Authorization": TokenApp.tokenApp!,
+      "Content-Type": "application/json"
+    }, body: {
+      "id": idMaterial,
+      "qtd": novaQuantidade
+    });
   }
 }
