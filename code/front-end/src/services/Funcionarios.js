3e08a6c5563001bfebdeda7d8dca92d96e2d42b6
xMarih <marry-29@hotmail.com>
2024-11-25 17:10:00 -0300

    integração back end funcionarios

--- code/front-end/src/services/Funcionarios.js
+++ code/front-end/src/services/Funcionarios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import { API_BASE_URL } from './config'; 
+import { API_BASE_URL } from './config';
 
-const API_URL_MATERIAL = `${API_BASE_URL}/materials`;
+const API_URL_USER = `${API_BASE_URL}/users`;
+const API_URL_AUTH = `${API_BASE_URL}/auth`;
 
 const getAuthHeader = () => {
   const token = localStorage.getItem('authToken');
@@ -9,86 +10,123 @@ const getAuthHeader = () => {
 };
 
 export default {
-  async getMaterials() {
+  async getCurrentUserProfile() {
     try {
-      const response = await axios.get(API_URL_MATERIAL, {
+      const response = await axios.get(`${API_URL_USER}/profile`, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao obter material:', error);
+      console.error('Erro ao obter o perfil do usuário atual:', error);
       throw error;
     }
   },
 
-  async addMaterial(materialData) {
+  async updateUser(userData) {
     try {
-      const response = await axios.post(API_URL_MATERIAL, materialData, {
+      const response = await axios.put(`${API_URL_USER}`, userData, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao adicionar material:', error);
+      console.error('Erro ao atualizar dados do usuário:', error);
       throw error;
     }
   },
 
-  async updateMaterial(id, materialData) {
+  async getAllUsers() {
     try {
-      const response = await axios.put(`${API_URL_MATERIAL}/${id}`, materialData, {
+      const response = await axios.get(`${API_URL_USER}/all`, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao atualizar material:', error);
+      console.error('Erro ao obter todos os usuários:', error);
       throw error;
     }
   },
 
-  async getMaterialById(id) {
+  async getUserById(id) {
     try {
-      const response = await axios.get(`${API_URL_MATERIAL}/${id}`, {
+      const response = await axios.get(`${API_URL_USER}/${id}`, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao obter material pelo ID:', error);
+      console.error('Erro ao obter o usuário pelo ID:', error);
       throw error;
     }
   },
 
-  async deleteMaterial(id) {
+  async deleteUser(id) {
     try {
-      const response = await axios.delete(`${API_URL_MATERIAL}/${id}`, {
+      const response = await axios.delete(`${API_URL_USER}/${id}`, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao deletar material:', error);
+      console.error('Erro ao deletar o usuário:', error);
       throw error;
     }
   },
 
-  async updateMaterialStock(id, qtd) {
+  async getUserByUsername(username) {
     try {
-      const response = await axios.patch(`${API_URL_MATERIAL}/stock/update`, { id, qtd }, {
+      const response = await axios.get(`${API_URL_USER}/username/${username}`, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao atualizar estoque do material:', error);
+      console.error('Erro ao obter o usuário pelo nome de usuário:', error);
       throw error;
     }
   },
 
-  async addMaterialStock(id, qtd) {
+  async getUserByEmail(email) {
     try {
-      const response = await axios.patch(`${API_URL_MATERIAL}/stock/add`, { id, qtd }, {
+      const response = await axios.get(`${API_URL_USER}/email/${email}`, {
         headers: getAuthHeader(),
       });
       return response.data;
     } catch (error) {
-      console.error('Erro ao adicionar material ao estoque:', error);
+      console.error('Erro ao obter o usuário pelo e-mail:', error);
+      throw error;
+    }
+  },
+
+  async getUsersByFilters(filters) {
+    try {
+      const response = await axios.get(`${API_URL_USER}/filters`, {
+        headers: getAuthHeader(),
+        params: filters,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao obter usuários com filtros:', error);
+      throw error;
+    }
+  },
+
+  // Adicionar novo usuário
+  async registerUser(userData) {
+    try {
+      const response = await axios.post(`${API_URL_AUTH}/register`, userData, {
+        headers: getAuthHeader(),
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao registrar um novo usuário:', error);
+      throw error;
+    }
+  },
+
+  // Login do usuário
+  async loginUser(credentials) {
+    try {
+      const response = await axios.post(`${API_URL_AUTH}/login`, credentials);
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
       throw error;
     }
   },
